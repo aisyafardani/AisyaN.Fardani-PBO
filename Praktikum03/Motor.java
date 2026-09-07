653e6d7bd0c2feefb3b5d9463897580e83f67862
Aisyanf <an.fardani@gmail.com>
2026-09-07 09:37:37 +0700

    Membuat Mode Private

--- Praktikum03/Motor.java
+++ Praktikum03/Motor.java
@@ -1,9 +1,33 @@
 package Praktikum03;
 
 public class Motor {
-    public String platNomor;
-    public boolean statusMesin;
-    public int kecepatan;
+    private String platNomor;
+    private boolean statusMesin;
+    private int kecepatan;
+
+    public String getPatNomor() {
+        return platNomor;
+    }
+
+    public void setPlatNomor(String platNomor) {
+        this.platNomor = platNomor;
+    }
+
+    public boolean getStatusMesin() {
+        return statusMesin;
+    }
+
+    public void setStatusMesin (boolean statusMesin) {
+        this.statusMesin = statusMesin;
+    }
+
+    public int getKecepatan() {
+        return kecepatan;
+    }
+
+    public void setKecepatan(int kecepatan) {
+        this.kecepatan = kecepatan;
+    }
 
     public void displayInformasi() {
         System.out.println("Plat Nomor: " +this.platNomor);
